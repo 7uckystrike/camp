5d49538211cc3443ad102eb54344448bb6a07b28
yeoung <yeoung.dev@gmail.com>
2023-03-21 22:19:35 +0900

    프리캠프 10일차

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -13,11 +13,17 @@ function counterMaker() {
     const nowDate = new Date();
     const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0) //자정기준
     const remaining = (targetDate - nowDate) / 1000
+      // 만약 리메이닝이 0이라면, 타이머가 종료 출력
+      if(remaining < 0) {
+        console.log('타이머가 종료되었습니다.')
+      } else if(isNaN(remaining)) {
+        console.log('유효한 시간대가 아닙니다.')
+      }
+
+    
     const remainingData = Math.floor(remaining / 3600 / 24)
     const remainingHours = Math.floor(remaining / 3600) % 24
     const remainingMin = Math.floor(remaining / 60) % 60
     const remainingSec = Math.floor(remaining / 60) % 60
-
-    console.log(remainingData, remainingHours, remainingMin, remainingSec)
 }
 
